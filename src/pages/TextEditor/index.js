b37c80f193cef064c0476e499e0be97db2231939
Christophe HEISER <ptit_tof57@hotmail.fr>
2018-10-31 13:34:41 +0100

    Make it easier to understand where to write code for new players

--- src/pages/TextEditor/index.js
+++ src/pages/TextEditor/index.js
@@ -17,7 +17,7 @@ import './TextEditor.scss';
 
 const initialState = {
   textEditorContent: `var solveChallenge = function(x) {
-  return "lol";
+    // Type your code goes here
 }`,
   room: {
     id: 0,
